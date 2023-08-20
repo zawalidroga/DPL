4254f2b3e321b9e6f59f5791033a9858c8bd17b1
Michal Walo <michal.walo.8@gmail.com>
2023-08-20 23:40:38 +0200

    firsst commit

---
+++ DPL/src/App.jsx
@@ -0,0 +1,13 @@
+//import { useState } from "react";
+import "./App.css";
+import DataInput from "./components/dataInput";
+
+function App() {
+  return (
+    <>
+      <DataInput />
+    </>
+  );
+}
+
+export default App;
